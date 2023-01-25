2dc7447864447f2328c429668a3aacb3d219be5d
0866 <0866playback@gmail.com>
2023-01-25 11:47:22 -0800

    Remove redundant code, localize library

--- ci/include/runtime.lua
+++ ci/include/runtime.lua
@@ -2,77 +2,51 @@ local instances = {}
 local modules = {}
 local currentlyLoading = {}
 
----Gets the globals table for a module. Stored globally to be accessed by
----a chunk loaded in `loadstring`.
----@param path string
----@return table
-function _G.__getGlobals(path)
-	return modules[instances[path]].globals
-end
+local function runModule(object, context)
+	currentlyLoading[context] = object
 
----Throws an error if the module may cause a circular dependency. Returns a
----cleanup function to run if the module is loaded successfully.
----@param module ModuleScript
----@param callerPath string
----@return nil | fun():void cleanup
-local function validateRequire(module, callerPath)
-	currentlyLoading[callerPath] = module
-
-	local currentModule = module
+	local currentModule = object
 	local depth = 0
 
-	-- If the module is loaded, requiring it will not cause a circular dependency.
-	if not modules[module] then
-		while currentModule do
-			depth = depth + 1
-			currentModule = currentlyLoading[currentModule]
+	while currentModule do
+		depth = depth + 1
+		currentModule = currentlyLoading[currentModule]
 
-			if currentModule == module then
-				local str = currentModule.Name -- Get the string traceback
+		if currentModule == object then
+			local str = currentModule.Name -- Get the string traceback
 
-				for _ = 1, depth do
-					currentModule = currentlyLoading[currentModule]
-					str = str .. "  ⇒ " .. currentModule.Name
-				end
-
-				error("Failed to load '" .. module.Name .. "'; Detected a circular dependency chain: " .. str, 2)
+			for _ = 1, depth do
+				currentModule = currentlyLoading[currentModule]
+				str = str .. "  ⇒ " .. currentModule.Name
 			end
+
+			error("Failed to load '" .. object.Name .. "'; Detected a circular dependency chain: " .. str, 2)
 		end
 	end
 
-	return function()
-		if currentlyLoading[callerPath] == module then -- Thread-safe cleanup!
-			currentlyLoading[callerPath] = nil
-		end
+	local module = modules[object]
+	local data = module.callback()
+
+	if currentlyLoading[context] == object then -- Thread-safe cleanup!
+		currentlyLoading[context] = nil
 	end
+
+	return data
 end
 
----Requires a module and returns the result.
----@param object ModuleScript
----@param callerPath string
----@return any
-local function requireModule(object, callerPath)
+local function requireModule(object, context)
 	local module = modules[object]
 
 	if module.loaded then
 		return module.result
 	else
-		local cleanup = validateRequire(object, callerPath)
-		module.result = module.callback()
+		module.result = runModule(object, context)
 		module.loaded = true
-		cleanup()
 		return module.result
 	end
 end
 
----Creates a new instance and adds it to the instances table. Called by
----the generated code.
----@param name string
----@param className string
----@param path string
----@param parentPath string
----@return Instance
-function __instance(name, className, path, parentPath)
+local function __rbx(name, className, path, parentPath)
 	local rbx = Instance.new(className)
 	rbx.Name = name
 	rbx.Parent = instances[parentPath]
@@ -80,14 +54,8 @@ function __instance(name, className, path, parentPath)
 	return rbx
 end
 
----Creates a new module and adds it to the modules table. Called by
----the generated code.
----@param name string
----@param className string
----@param path string
----@param parentPath string
-function __lua(name, className, path, parentPath, callback)
-	local rbx = __instance(name, className, path, parentPath)
+local function __lua(name, className, path, parentPath, callback)
+	local rbx = __rbx(name, className, path, parentPath)
 
 	modules[rbx] = {
 		callback = callback,
@@ -106,8 +74,11 @@ function __lua(name, className, path, parentPath, callback)
 	}
 end
 
----Starts the project.
-function __start()
+local function __env(path)
+	return modules[instances[path]].globals
+end
+
+local function __start()
 	for rbx, module in pairs(modules) do
 		if rbx.ClassName == "LocalScript" and not rbx.Disabled then
 			task.spawn(module.callback)
